47d68a88cdfee1cf2f2e30c8a937bd2f18a14d9b
jhuancal <jhuancal@unsa.edu.pe>
2026-01-12 03:34:39 -0500

    correcciones v3

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 # Configuración SMTP
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'jhuancal@unsa.edu.pe'
 app.config['MAIL_PASSWORD'] = 'pghhwptfugmwcscn'
